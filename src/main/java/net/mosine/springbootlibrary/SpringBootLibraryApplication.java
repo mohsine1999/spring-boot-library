07a862c226cad6ab996c2f7d8364dcc02795e3f3
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-08 23:34:11 +0100

    application.properties to application.yml

--- src/main/java/net/mosine/springbootlibrary/SpringBootLibraryApplication.java
+++ src/main/java/net/mosine/springbootlibrary/SpringBootLibraryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLibraryApplication.class, args);
+    }
 
 }
