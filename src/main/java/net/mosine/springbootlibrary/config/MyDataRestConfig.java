7efd72c538e4b98a1638877c1afdb667ed3f18ba
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-09 01:34:55 +0100

    Book Entity & Only Read Methods

---
+++ src/main/java/net/mosine/springbootlibrary/config/MyDataRestConfig.java
@@ -0,0 +1,36 @@
+package net.mosine.springbootlibrary.config;
+
+import net.mosine.springbootlibrary.entities.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    public void configureRepositoryRestConfiguration (RepositoryRestConfiguration configuration, CorsRegistry cors){
+        HttpMethod [] theUnsupporttedActions = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE,
+                HttpMethod.PUT
+        };
+    configuration.exposeIdsFor(Book.class);
+
+    disableHttpMethods(Book.class, configuration, theUnsupporttedActions);
+    /* Configure CORS Mapping */
+        cors.addMapping(configuration.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    private void disableHttpMethods(Class bookClass, RepositoryRestConfiguration configuration, HttpMethod[] theUnsupporttedActions) {
+        configuration.getExposureConfiguration()
+                     .forDomainType(bookClass)
+                     .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupporttedActions)))
+                     .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupporttedActions));
+    }
+}
---
+++ src/main/java/net/mosine/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,8 @@
+package net.mosine.springbootlibrary.dao;
+
+import net.mosine.springbootlibrary.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository< Book, Long> {
+
+}
