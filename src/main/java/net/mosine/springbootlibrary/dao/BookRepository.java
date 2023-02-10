80e8c7c9d38997b9ffeddb76442a8d8a68f78f39
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-10 17:52:11 +0100

    Search By title

--- src/main/java/net/mosine/springbootlibrary/dao/BookRepository.java
+++ src/main/java/net/mosine/springbootlibrary/dao/BookRepository.java
@@ -1,8 +1,12 @@
 package net.mosine.springbootlibrary.dao;
 
 import net.mosine.springbootlibrary.entities.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository< Book, Long> {
 
+    Page<Book> findByTitleContaining(@RequestParam("title") String title , Pageable pageable);
 }
